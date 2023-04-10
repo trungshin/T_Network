f00d62215cca128e951e6cb54e779a7e64664c76
trungshin <trungviet6969@gmail.com>
2023-04-11 01:35:56 +0700

    fix account menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,42 +1,34 @@
-import React, { useState } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { styled } from "@mui/material/styles";
 import { NavLink, useNavigate } from "react-router-dom";
-import { Box, Toolbar, IconButton, Avatar, Typography, Menu, MenuItem } from "@mui/material";
+import { Toolbar, IconButton, Avatar, Menu, MenuItem, Tooltip } from "@mui/material";
 import MuiAppBar from "@mui/material/AppBar";
 import MenuIcon from "@mui/icons-material/Menu";
+import ListItemIcon from "@mui/material/ListItemIcon";
 import { sideBarToggle } from "../redux/navigateSlice";
 import { logOutUser } from "../redux/apiRequests";
 import SearchUser from "./Search";
+import Logout from "@mui/icons-material/Logout";
 
 const AppBar = styled(MuiAppBar)(({ theme }) => ({
 	zIndex: theme.zIndex.drawer + 1
 }));
 
-const IconButtons = styled(Box)(({ theme }) => ({
-	display: "none",
-	alignItems: "center",
-	gap: "20px",
-	[theme.breakpoints.up("sm")]: {
-		display: "flex"
-	}
-}));
-
-const UserBox = styled(Box)(({ theme }) => ({
-	display: "flex",
-	alignItems: "center",
-	gap: "10px",
-	[theme.breakpoints.up("sm")]: {
-		display: "none"
-	}
-}));
-
 const NavBar = () => {
 	const dispatch = useDispatch();
-	const [openMenu, setOpenMenu] = useState(false);
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const open = Boolean(anchorEl);
 	const user = useSelector((state) => state.user.user?.currentUser);
 	const isOpen = useSelector((state) => state.nav.sidebar.open);
 	const navigate = useNavigate();
+
+	const handleClick = (event) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
 
 	const toggleDrawer = () => {
 		dispatch(sideBarToggle(!isOpen));
@@ -55,33 +47,66 @@ const NavBar = () => {
 
 				<SearchUser user={user} />
 
-				<IconButtons>
-					<IconButton size="medium" edge="end" aria-label="account of current user" color="inherit">
-						<Avatar
-							sx={{ width: 30, height: 30 }}
-							alt={user?.username}
-							src={user?.avatar}
-							onClick={(e) => setOpenMenu(true)}
-						/>
+				<Tooltip title="Account settings">
+					<IconButton
+						onClick={handleClick}
+						size="medium"
+						sx={{ ml: 2 }}
+						aria-controls={open ? "account-menu" : undefined}
+						aria-haspopup="true"
+						aria-expanded={open ? "true" : undefined}
+					>
+						<Avatar sx={{ width: 32, height: 32 }} alt={user?.username} src={user?.avatar} />
 					</IconButton>
-				</IconButtons>
-				<UserBox onClick={(e) => setOpenMenu(true)}>
-					<Avatar sx={{ width: 30, height: 30 }} alt={user?.username} src={user?.avatar} />
-					<Typography variant="span">{user?.username}</Typography>
-				</UserBox>
+				</Tooltip>
 			</Toolbar>
 			<Menu
-				id="menu"
-				aria-labelledby="button"
-				open={openMenu}
-				onClose={(e) => setOpenMenu(false)}
+				anchorEl={anchorEl}
+				id="account-menu"
+				open={open}
+				onClose={handleClose}
+				onClick={handleClose}
+				PaperProps={{
+					elevation: 0,
+					sx: {
+						overflow: "visible",
+						filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+						mt: 1.5,
+						"& .MuiAvatar-root": {
+							width: 32,
+							height: 32,
+							ml: -0.5,
+							mr: 1
+						},
+						"&:before": {
+							content: '""',
+							display: "block",
+							position: "absolute",
+							top: 0,
+							right: 14,
+							width: 10,
+							height: 10,
+							bgcolor: "background.paper",
+							transform: "translateY(-50%) rotate(45deg)",
+							zIndex: 0
+						}
+					}
+				}}
 				transformOrigin={{ horizontal: "right", vertical: "top" }}
 				anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
 			>
 				<NavLink to={`/user/${user?._id}`} style={{ textDecoration: "none", color: "unset" }}>
-					<MenuItem>Profile</MenuItem>
+					<MenuItem>
+						<Avatar sx={{ width: 32, height: 32 }} alt={user?.username} src={user?.avatar} />{" "}
+						{user?.username}
+					</MenuItem>
 				</NavLink>
-				<MenuItem onClick={logOut}>Logout</MenuItem>
+				<MenuItem onClick={logOut}>
+					<ListItemIcon>
+						<Logout fontSize="small" />
+					</ListItemIcon>
+					Logout
+				</MenuItem>
 			</Menu>
 		</AppBar>
 	);
