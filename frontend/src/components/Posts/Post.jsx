29478ee04b21293585d760c9e8415701e347d229
trungshin <trungviet6969@gmail.com>
2023-04-20 13:05:51 +0700

    fix isAdmin

--- frontend/src/components/Posts/Post.jsx
+++ frontend/src/components/Posts/Post.jsx
@@ -167,7 +167,7 @@ const Post = ({ post }) => {
 						</NavLink>
 					}
 					action={
-						<React.Fragment>
+						<>
 							<Box
 								sx={{
 									display: "flex",
@@ -186,59 +186,61 @@ const Post = ({ post }) => {
 									<MoreVert />
 								</IconButton>
 							</Box>
-							<Menu
-								anchorEl={anchorEl}
-								id="account-menu-2"
-								open={open}
-								onClose={handleClose}
-								onClick={handleClose}
-								PaperProps={{
-									elevation: 0,
-									sx: {
-										overflow: "visible",
-										filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-										mt: 1.5,
-										"& .MuiAvatar-root": {
-											width: 32,
-											height: 32,
-											ml: -0.5,
-											mr: 1
-										},
-										"&:before": {
-											content: '""',
-											display: "block",
-											position: "absolute",
-											top: 0,
-											right: 14,
-											width: 10,
-											height: 10,
-											bgcolor: "background.paper",
-											transform: "translateY(-50%) rotate(45deg)",
-											zIndex: 0
+							{(user?._id === post?.userId || user?.admin) && (
+								<Menu
+									anchorEl={anchorEl}
+									id="account-menu-2"
+									open={open}
+									onClose={handleClose}
+									onClick={handleClose}
+									PaperProps={{
+										elevation: 0,
+										sx: {
+											overflow: "visible",
+											filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+											mt: 1.5,
+											"& .MuiAvatar-root": {
+												width: 32,
+												height: 32,
+												ml: -0.5,
+												mr: 1
+											},
+											"&:before": {
+												content: '""',
+												display: "block",
+												position: "absolute",
+												top: 0,
+												right: 14,
+												width: 10,
+												height: 10,
+												bgcolor: "background.paper",
+												transform: "translateY(-50%) rotate(45deg)",
+												zIndex: 0
+											}
 										}
-									}
-								}}
-								transformOrigin={{ horizontal: "right", vertical: "top" }}
-								anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
-							>
-								<MenuItem
-									onClick={() => handleDelete(post?._id)}
-									className="item"
-									sx={{ color: "#333" }}
+									}}
+									transformOrigin={{ horizontal: "right", vertical: "top" }}
+									anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
 								>
-									<DeleteOutlined color="#333" sx={{ marginRight: "5px" }} /> Remove Post
-								</MenuItem>
-								{user?._id === post?.userId && (
 									<MenuItem
-										onClick={() => handleClickOpen("paper")}
+										onClick={() => handleDelete(post?._id)}
 										className="item"
 										sx={{ color: "#333" }}
 									>
-										<EditOutlined color="#333" sx={{ marginRight: "5px" }} /> Edit Post
+										<DeleteOutlined color="#333" sx={{ marginRight: "5px" }} /> Remove Post
 									</MenuItem>
-								)}
-							</Menu>
-						</React.Fragment>
+									{user?._id === post?.userId && (
+										<MenuItem
+											onClick={() => handleClickOpen("paper")}
+											className="item"
+											sx={{ color: "#333" }}
+										>
+											<EditOutlined color="#333" sx={{ marginRight: "5px" }} /> Edit Post
+										</MenuItem>
+									)}
+								</Menu>
+							)}
+						</>
 					}
 					title={
 						<Link
